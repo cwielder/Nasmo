d01e943d6e01ba06c13bbdbb42e2bf45db621b4b
cwielder <160283747+cwielder@users.noreply.github.com>
2024-06-06 17:18:11 -0400

    Add some optimization ideas as comments

--- Nasmo/src/gfx/layer/modellayer.cpp
+++ Nasmo/src/gfx/layer/modellayer.cpp
@@ -20,7 +20,7 @@ nsm::ModelLayer::ModelLayer(const std::string& name)
 void nsm::ModelLayer::draw(const RenderInfo& renderInfo) {
     mRenderState.apply();
 
-    std::unordered_set<Model*> models;
+    std::unordered_set<Model*> models; // TODO: Keep sets of models that use the same shader, so we can batch them together, and then sort by material to batch those too
     for (const auto& component : mDrawables) {
         auto modelComponent = dynamic_cast<ModelComponent*>(component);
         if (modelComponent) {
