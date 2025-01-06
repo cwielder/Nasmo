0ab0ec02810b81c6b556842ea593747afed645e4
cwielder <160283747+cwielder@users.noreply.github.com>
2025-01-05 19:14:23 -0500

    Add InputComponent for getting keyboard/mouse

--- Sandbox/src/entity/playerentity.cpp
+++ Sandbox/src/entity/playerentity.cpp
@@ -4,6 +4,7 @@
 #include <nsm/entity/component/modelcomponent.h>
 #include <nsm/entity/component/transformcomponent.h>
 #include <nsm/entity/component/particlecomponent.h>
+#include <nsm/entity/component/inputcomponent.h>
 #include <nsm/util/jsonhelpers.h>
 
 #include <glm/gtc/matrix_transform.hpp>
@@ -56,27 +57,82 @@ public:
         this->addComponent<nsm::DrawableComponent>(mExhaustParticleRight);
         CreateExhaustParticle(mExhaustParticleRight);
 
-        mModel->setTransformAll(glm::scale(glm::translate(glm::mat4(1.0f), mTransform->getPosition()), mTransform->getScale()));
+        static constexpr f32 cAcceleration = 0.25f;
+
+        mInput = new nsm::InputComponent();
+        this->addComponent<nsm::InputComponent>(mInput);
+        mInput->setOnKeyPress([&](const nsm::KeyPressEvent* event) {
+            switch (event->getKey()) {
+                case GLFW_KEY_A:
+                case GLFW_KEY_LEFT: {
+                    mAcceleration = -cAcceleration;
+                    break;
+                }
+
+                case GLFW_KEY_D:
+                case GLFW_KEY_RIGHT: {
+                    mAcceleration = cAcceleration;
+                    break;
+                }
+            }
+        });
+
+        mInput->setOnKeyRelease([&](const nsm::KeyReleaseEvent* event) {
+            switch (event->getKey()) {
+                case GLFW_KEY_A:
+                case GLFW_KEY_LEFT:
+                case GLFW_KEY_D:
+                case GLFW_KEY_RIGHT: {
+                    mAcceleration = 0.0f;
+                    break;
+                }
+            }
+        });
+
+        glm::mat4 mtx = glm::mat4(1.0f);
+        mtx = glm::translate(mtx, mTransform->getPosition());
+        mtx = glm::rotate(mtx, glm::radians(mVelocity), glm::vec3(1.0f, 1.0f, 0.0f));
+        mtx = glm::scale(mtx, mTransform->getScale());
+
+        mModel->setTransformAll(mtx);
     }
 
     void onUpdate(const f64 timeStep) override {
         nsm::ParticleEmitter& emitter = mExhaustParticleLeft->getEmitter();
-        
-        glm::vec3 scale = mTransform->getScale();
-        glm::vec3 position = mTransform->getPosition();
-        
-        if (ImGui::Begin("Demo Entity")) {
-            ImGui::DragFloat3("Scale", &scale.x, 0.1f);
-            ImGui::DragFloat3("Position", &position.x, 0.1f);
-        } ImGui::End();
 
-        mTransform->setScale(scale);
+        static constexpr f32 cMaxVelocity = 0.06f;
+
+        if (mAcceleration != 0.0f) {
+            mVelocity += mAcceleration * timeStep;
+            mVelocity = glm::clamp(mVelocity, -cMaxVelocity, cMaxVelocity);
+        }
+
+        // Friction
+        static constexpr f32 cFriction = 0.1f;
+        if (mVelocity > 0.0f) {
+            mVelocity -= cFriction * timeStep;
+            mVelocity = glm::max(mVelocity, 0.0f);
+        } else if (mVelocity < 0.0f) {
+            mVelocity += cFriction * timeStep;
+            mVelocity = glm::min(mVelocity, 0.0f);
+        }
+
+        nsm::info("Velocity: ", mVelocity);
+
+        glm::vec3 position = mTransform->getPosition();
+        position += glm::vec3(0.0f, 0.0f, mVelocity);
+
         mTransform->setPosition(position);
 
         mExhaustParticleLeft->getEmitter().setPosition(position + glm::vec3(-7.0f, -0.5f, -4.0f));
         mExhaustParticleRight->getEmitter().setPosition(position + glm::vec3(-7.0f, -0.5f, 4.0f));
 
-        mModel->setTransformAll(glm::scale(glm::translate(glm::mat4(1.0f), mTransform->getPosition()), mTransform->getScale()));
+        glm::mat4 mtx = glm::mat4(1.0f);
+        mtx = glm::translate(mtx, mTransform->getPosition());
+        mtx = glm::rotate(mtx, glm::radians(-mVelocity * 400.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+        mtx = glm::scale(mtx, mTransform->getScale());
+
+        mModel->setTransformAll(mtx);
     }
 
 private:
@@ -85,6 +141,10 @@ private:
     nsm::ModelComponent* mModel;
     nsm::ParticleComponent* mExhaustParticleLeft;
     nsm::ParticleComponent* mExhaustParticleRight;
+    nsm::InputComponent* mInput;
+
+    f32 mAcceleration = 0.0f;
+    f32 mVelocity = 0.0f;
 };
 
 NSM_REGISTER_ENTITY(PlayerEntity);
